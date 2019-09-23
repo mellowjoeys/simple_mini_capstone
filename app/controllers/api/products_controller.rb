3bb73d747556e6f576d62021e6b7dcb3fb09431b
JackMarx <seriousfools@gmail.com>
2019-09-23 14:27:17 -0500

    initial commit

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,47 @@
+class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render 'index.json.jb'
+  end
+
+  def create
+    @product = Product.new(
+                           name: params[:name],
+                           price: params[:price],
+                           description: params[:description],
+                           image_url: params[:image_url]
+                          )
+
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Successfully Destroyed Product"}
+  end
+end
